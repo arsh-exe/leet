30b431671ec1618cd5f9c2c25fd59fc179f1cee5
Arsh5o4 <86293110+arsh-exe@users.noreply.github.com>
2026-07-12 00:13:42 +0530

    Time: 55 ms (45.96%), Space: 128.5 MB (71.93%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>>adj;
+    vector<int>vis;
+    int nodes;
+    int degreeSum;
+    void dfs(int u){
+        vis[u] = 1;
+        nodes++;
+        degreeSum +=adj[u].size();
+        for(int v: adj[u]){
+            if(!vis[v])
+                dfs(v);
+        }
+    }
+
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        adj.assign(n,{});
+        vis.assign(n,0);
+        for(auto &e: edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+
+        }
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            if(!vis[i]){
+                nodes = 0;
+                degreeSum = 0;
+                dfs(i);
+                int  edgeCount = degreeSum/2;
+                if(edgeCount==nodes*(nodes-1)/2) ans++;
+
+            }
+        }
+        return ans;
+    }
+};
