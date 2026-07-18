bdb4c02f505f6eadb891fc6d203df2a8287c0561
Arsh5o4 <86293110+arsh-exe@users.noreply.github.com>
2026-07-18 10:29:48 +0530

    Time: 0 ms (100%), Space: 8.9 MB (6.68%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,32 +1,36 @@
-// class Solution {
-// public:
-//     double myPow(double x, int n) {
-//         double ans = pow(x,n);
-//         return ans;
-//     }
-// };
 class Solution {
 public:
-    double power(double x, long long n) {
-        if (n == 0)
-            return 1;
-
-        double half = power(x, n / 2);
-
-        if (n % 2 == 0)
-            return half * half;
-
-        return x * half * half;
-    }
-
     double myPow(double x, int n) {
-        long long N = n;
+        double ans = pow(x,n);
+        return ans;
+        // if(n == 0) return 1;
+        // if(n<0) return 1/myPow(x,-n);
+        // return x*myPow(x,n-1);
 
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-
-        return power(x, N);
     }
 };
+// class Solution {
+// public:
+//     double power(double x, long long n) {
+//         if (n == 0)
+//             return 1;
+
+//         double half = power(x, n / 2);
+
+//         if (n % 2 == 0)
+//             return half * half;
+
+//         return x * half * half;
+//     }
+
+//     double myPow(double x, int n) {
+//         long long N = n;
+
+//         if (N < 0) {
+//             x = 1 / x;
+//             N = -N;
+//         }
+
+//         return power(x, N);
+//     }
+// };
